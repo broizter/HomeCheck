97841c2e9796e9e9a546c835f2071127640c6846
Homerocker <molodoy3561@gmail.com>
2025-08-24 01:02:45 +0300

    disable title bar by default when upgrading from previous versions of addon to prevent unintended interface changes

--- HomeCheck/Core.lua
+++ HomeCheck/Core.lua
@@ -12,7 +12,7 @@ HomeCheck.groups = {}
 HomeCheck.localizedSpellNames = {}
 HomeCheck.deadUnits = {}
 HomeCheck.RebirthTargets = {}
-HomeCheck.db_ver = 4
+HomeCheck.db_ver = 5
 
 HomeCheck.comms = {
     oRA = "oRA",
